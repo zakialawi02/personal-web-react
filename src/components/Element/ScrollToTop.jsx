cf4785f2862be7087a5cee62353e62b270188624
Ahmad zaki Alawi <zaki.alawi22@gmail.com>
2024-07-28 21:51:06 +0700

    fix scroll to top

--- src/components/Element/ScrollToTop.jsx
+++ src/components/Element/ScrollToTop.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+
 const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisible = () => {
+      if (window.pageYOffset > 200) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("scroll", toggleVisible);
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
+  }, []);
+
   return (
-    <a href="#" className="fixed text-2xl bottom-[7.5rem] right-[2rem] z-[100] animate-bounceHero6s">
+    <a href="#" className={`${isVisible ? "fixed" : "hidden"} text-2xl bottom-[7.5rem] right-[2rem] z-[100] animate-bounceHero6s animate-scaleUp`}>
       <i className="px-3 py-4 shadow-lg fas fa-arrow-up bg-light text-primary rounded-2xl dark:text-dark-secondary"></i>
     </a>
   );
