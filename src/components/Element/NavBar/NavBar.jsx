a744d27dac3fc7a0e3e20465707e0a8d838fa3b1
Ahmad zaki Alawi <zaki.alawi22@gmail.com>
2024-10-10 06:59:49 +0700

    fix css nav menu

--- src/components/Element/NavBar/NavBar.jsx
+++ src/components/Element/NavBar/NavBar.jsx
@@ -1,74 +1,91 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavLogo from "./NavLogo";
 import NavMenu from "./NavMenu";
 
 const NavBar = () => {
-  const [navBarMobile, setNavBarMobile] = useState(false);
-  const [isSticky, setIsSticky] = useState(false);
+    const [navBarMobile, setNavBarMobile] = useState(false);
+    const [isSticky, setIsSticky] = useState(false);
+    const navBarRef = useRef(null);
 
-  useEffect(() => {
-    const handleResize = () => {
-      const navMenu = document.getElementById("navMenu");
-      const width = window.innerWidth;
-      const isMobile = width < 780;
+    const toggleMenu = () => {
+        const navMenu = document.getElementById("navMenu");
+        const isOpen = navBarMobile && navMenu.classList.contains("scale-y-100");
 
-      navMenu.classList.toggle("scale-y-100", !isMobile);
-      navMenu.classList.toggle("opacity-100", !isMobile);
-      setNavBarMobile(isMobile);
+        navMenu.classList.toggle("scale-y-100", !isOpen);
+        navMenu.classList.toggle("opacity-100", !isOpen);
+        navMenu.classList.toggle("scale-y-0", isOpen);
+        navMenu.classList.toggle("opacity-0", isOpen);
+
+        setNavBarMobile(!isOpen);
     };
 
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const stickyThreshold = 50;
-      setIsSticky(scrollY > stickyThreshold);
+    const menuClicked = () => {
+        const navMenu = document.getElementById("navMenu");
+        const isOpen = navBarMobile && navMenu.classList.contains("scale-y-100");
+        if (isOpen) {
+            toggleMenu();
+        }
     };
 
-    window.addEventListener("resize", handleResize);
-    handleResize();
-
-    window.addEventListener("scroll", handleScroll);
-    handleScroll();
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-      window.removeEventListener("scroll", handleScroll);
+    const handleClickOutside = (event) => {
+        if (navBarRef.current && !navBarRef.current.contains(event.target)) {
+            const navMenu = document.getElementById("navMenu");
+            if (navBarMobile && navMenu.classList.contains("scale-y-100")) {
+                toggleMenu(); // Close the menu if it's open and the click is outside
+            }
+        }
     };
-  }, []);
 
-  const toggleMenu = () => {
-    const navMenu = document.getElementById("navMenu");
-    const isOpen = navBarMobile && navMenu.classList.contains("scale-y-100");
+    useEffect(() => {
+        const handleResize = () => {
+            const navMenu = document.getElementById("navMenu");
+            const width = window.innerWidth;
+            const isMobile = width < 780;
 
-    navMenu.classList.toggle("scale-y-100", !isOpen);
-    navMenu.classList.toggle("opacity-100", !isOpen);
-    navMenu.classList.toggle("scale-y-0", isOpen);
-    navMenu.classList.toggle("opacity-0", isOpen);
+            navMenu.classList.toggle("scale-y-100", !isMobile);
+            navMenu.classList.toggle("opacity-100", !isMobile);
+            navMenu.classList.toggle("shadow-lg", isMobile);
+            setNavBarMobile(isMobile);
+        };
 
-    setNavBarMobile(!isOpen);
-  };
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const stickyThreshold = 50;
+            setIsSticky(scrollY > stickyThreshold);
+        };
 
-  const menuClicked = () => {
-    const navMenu = document.getElementById("navMenu");
-    const isOpen = navBarMobile && navMenu.classList.contains("scale-y-100");
-    if (isOpen) {
-      toggleMenu();
-    }
-  };
+        window.addEventListener("resize", handleResize);
+        handleResize();
 
-  return (
-    <header id="navBar" className={`z-[100] w-full bg-primary transition-all duration-500 ${isSticky ? "sticky top-0 shadow-md" : ""} transition-all duration-700 ease-in-out dark:bg-dark-primary`}>
-      <div id="navHead" className="z-10 flex items-center justify-between w-full px-6 py-3 min-h-18 md:min-h-20 lg:px-14">
-        <NavLogo className="inline-flex">
-          &#62;&#95;<span className={`C1 text-accent ${isSticky ? "hidden" : ""} dark:text-dark-light`}>&#34;</span>
-          <span className={`inline-flex C2 ${isSticky ? "" : "hidden"}`}>&#60;</span>
-        </NavLogo>
+        window.addEventListener("scroll", handleScroll);
+        handleScroll();
 
-        <div id="hamMenu" className="fas fa-bars" onClick={() => toggleMenu()}></div>
+        document.addEventListener("mousedown", handleClickOutside);
 
-        <NavMenu event={menuClicked} />
-      </div>
-    </header>
-  );
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            window.removeEventListener("scroll", handleScroll);
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [navBarMobile]);
+
+    return (
+        <header ref={navBarRef} id="navBar" className={`z-[100] w-full bg-primary transition-all duration-500 ${isSticky ? "sticky top-0 shadow-md" : ""} transition-all duration-700 ease-in-out dark:bg-dark-primary`}>
+            <div id="navHead" className="z-10 flex items-center justify-between w-full px-6 py-3 min-h-18 md:min-h-20 lg:px-14 font-Poppins">
+                <NavLogo className="inline-flex">
+                    {">_"}
+                    <span className={`C1 text-accent ${isSticky ? "hidden" : ""} dark:text-dark-light`}>{'"'}</span>
+                    <span className={`inline-flex C2 ${isSticky ? "" : "hidden"}`}>{"<"}</span>
+                </NavLogo>
+
+                <div id="hamMenu" className="fas fa-bars" onClick={() => toggleMenu()}></div>
+
+                {!navBarMobile && <NavMenu event={menuClicked} />}
+            </div>
+
+            {navBarMobile && <NavMenu event={menuClicked} />}
+        </header>
+    );
 };
 
 export default NavBar;
--- src/components/Element/NavBar/NavMenu.jsx
+++ src/components/Element/NavBar/NavMenu.jsx
@@ -1,40 +1,46 @@
 import MenuItem from "./MenuItem";
 
 const NavMenu = (props) => {
-  const { event } = props;
+    const { event } = props;
+    const { pathname } = window.location;
+    const isHome = pathname === "/";
 
-  return (
-    <div
-      id="navMenu"
-      className="absolute items-start md:items-center left-0 right-0 flex flex-col p-3 text-[1.1rem] md:w-[50rem] md:flex-wrap lg:w-full md:justify-end font-semibold md:relative top-20 md:flex-row md:opacity-100 scale-y-0 opacity-0 md:top-0 md:p-0 uppercase  md:bg-transparent z-[100] bg-primary text-light origin-top transition-all duration-300"
-    >
-      <MenuItem event={event} link="#home">
-        Home
-      </MenuItem>
-      <MenuItem event={event} link="https://zakialawi.my.id/blog">
-        Blog
-      </MenuItem>
-      <MenuItem event={event} link="#about">
-        About
-      </MenuItem>
-      <MenuItem event={event} link="#portfolio">
-        Portfolio
-      </MenuItem>
-      <MenuItem event={event} link="#contact">
-        Contact
-      </MenuItem>
-      <MenuItem
-        event={event}
-        link="https://gallery.zakialawi.my.id/"
-        className="px-4 mx-1 mb-2 shadow-xl rounded-2xl bg-accent hover:bg-primary hover:border-accent hover:border-2 hover:-translate-y-1 dark:bg-dark-accent dark:hover:border-dark-accent dark:hover:bg-dark-primary dark:hover:text-dark-light"
-      >
-        Gallery
-      </MenuItem>
-      <MenuItem event={event} link="https://zakialawi.my.id/login" className="px-4 mx-1 mb-2 shadow-xl rounded-2xl bg-error hover:bg-primary hover:border-error hover:border-2 hover:-translate-y-1 dark:hover:bg-dark-primary dark:hover:text-dark-light">
-        <i className="fa-solid fa-right-to-bracket"></i> Login
-      </MenuItem>
-    </div>
-  );
+    return (
+        <div
+            id="navMenu"
+            className="absolute items-start md:items-center left-0 right-0 flex flex-col p-3 text-[1.1rem] md:w-[50rem] md:flex-wrap lg:w-full md:justify-end font-semibold md:relative md:flex-row md:opacity-100 scale-y-0 opacity-0 md:top-0 md:p-0 uppercase  md:bg-transparent z-[100] bg-primary dark:bg-dark-primary text-light origin-top transition-all duration-300 shadow-xl md:shadow-none"
+        >
+            <MenuItem event={event} {...(isHome ? { link: "/#Home" } : { to: "/#Home" })}>
+                Home
+            </MenuItem>
+            <MenuItem event={event} link="https://zakialawi.com" target="_blank">
+                Blog
+            </MenuItem>
+            <MenuItem event={event} {...(isHome ? { link: "/#about" } : { to: "/#about" })}>
+                About
+            </MenuItem>
+            <MenuItem event={event} to="/my-portfolio">
+                Portfolio
+            </MenuItem>
+            <MenuItem event={event} {...(isHome ? { link: "/#contact" } : { to: "/#contact" })}>
+                Contact
+            </MenuItem>
+            <MenuItem
+                event={event}
+                link="https://gallery.zakialawi.my.id/"
+                className="px-4 mx-1 mb-2 shadow-xl rounded-2xl bg-accent hover:bg-primary hover:border-accent hover:border-2 hover:-translate-y-1 dark:bg-dark-accent dark:hover:border-dark-accent dark:hover:bg-dark-primary dark:hover:text-dark-light"
+            >
+                Gallery
+            </MenuItem>
+            <MenuItem
+                event={event}
+                link="https://zakialawi.my.id/login"
+                className="px-4 mx-1 mb-2 shadow-xl rounded-2xl bg-error hover:bg-primary hover:border-error hover:border-2 hover:-translate-y-1 dark:hover:bg-dark-primary dark:hover:text-dark-light"
+            >
+                <i className="fa-solid fa-right-to-bracket"></i> Login
+            </MenuItem>
+        </div>
+    );
 };
 
 export default NavMenu;
